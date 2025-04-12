ab7dc2f29cdc265fc79a7652bb188cf97459343b
seohyun <dlatjgus0528@naver.com>
2025-04-12 09:57:51 +0900

    Feat:프로젝트 생성, JPA 설정

---
+++ src/main/java/com/example/mind_laundromat/MindLaundromatApplication.java
@@ -0,0 +1,13 @@
+package com.example.mind_laundromat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindLaundromatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindLaundromatApplication.class, args);
+	}
+
+}

07b2c6cd6ffc4b1b074612c1f277d9560ba5ace0
seohyun <dlatjgus0528@naver.com>
2025-05-03 14:12:07 +0900

    Feat: Gemini 연결

---
+++ src/main/java/com/example/mind_laundromat/cbt/controller/GeminiController.java
@@ -0,0 +1,28 @@
+package com.example.mind_laundromat.cbt.controller;
+
+import org.springframework.ai.vertexai.gemini.VertexAiGeminiChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RequestMapping("/gemini")
+@RestController
+public class GeminiController {
+    private final VertexAiGeminiChatModel vertexAiGeminiChatModel;
+
+    @Autowired
+    public GeminiController(VertexAiGeminiChatModel chatModel) {
+        this.vertexAiGeminiChatModel = chatModel;
+    }
+
+    @GetMapping("/chat")
+    public Map<String, String> chat(@RequestBody String message) {
+        Map<String, String> responses = new HashMap<>();
+
+        String vertexAiGeminiResponse = vertexAiGeminiChatModel.call(message);
+        responses.put("vertexai(gemini) 응답", vertexAiGeminiResponse);
+        return responses;
+    }
+}
--- src/main/java/com/example/mind_laundromat/config/SecurityConfig.java
+++ src/main/java/com/example/mind_laundromat/config/SecurityConfig.java
@@ -2,37 +2,19 @@ package com.example.mind_laundromat.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable) // CSRF 비활성화
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/cbt/**").permitAll()
-                        .requestMatchers("/gemini/**").permitAll()
-                )
-                .httpBasic(Customizer.withDefaults())
-                .logout(Customizer.withDefaults()); // 로그아웃 설정
-
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
-
-    }
-
-
-    // 해시 함수 사용해서 패스워드 암호화
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
     }
 }

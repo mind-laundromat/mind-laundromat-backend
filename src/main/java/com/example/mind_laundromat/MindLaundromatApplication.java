3a180439d3ddffde9c5c3e1489fff81b47276137
seohyun <dlatjgus0528@naver.com>
2025-05-13 01:58:37 +0900

    chore: JVM UTC 시간대로 전환

--- src/main/java/com/example/mind_laundromat/MindLaundromatApplication.java
+++ src/main/java/com/example/mind_laundromat/MindLaundromatApplication.java
@@ -1,8 +1,11 @@
 package com.example.mind_laundromat;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -10,6 +13,12 @@ public class MindLaundromatApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MindLaundromatApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init(){
+		// Setting Spring Boot SetTimeZone
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 	}
 
 }
